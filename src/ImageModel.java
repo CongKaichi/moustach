eb2e9a62b3f557ded8dd8a4d58ba2555a6fd2377
KAICHI CONG <21709414@ad.ut-capitole.fr>
2018-10-03 12:04:04 +0200

    First releqse of Youstache!

---
+++ src/ImageModel.java
@@ -0,0 +1,26 @@
+
+import java.awt.Point;
+
+/**
+ * An image model : a file and the location of moustache
+ * @author David
+ */
+public class ImageModel {
+
+	/**
+	 * Image filename (without dir, without extension)
+	 */
+	String filename;
+
+	/**
+	 * Location of the moustache within the image
+	 * null if no moustache
+	 */
+	Point moustacheLocation; 
+	
+	public ImageModel(String f) {
+		this.filename = f;
+		this.moustacheLocation = null;
+	}
+
+}
